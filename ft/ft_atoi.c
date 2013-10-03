9b3484059f1b8c09a10cd31b938d17014fab7ca6
Leo VERNISSE <lverniss@e1r12p20.42.fr>
2013-10-03 21:42:30 +0200

    Maj

--- ft/ft_atoi.c
+++ ft/ft_atoi.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lverniss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/10/03 14:23:35 by lverniss          #+#    #+#             */
-/*   Updated: 2013/10/03 21:20:25 by lverniss         ###   ########.fr       */
+/*   Created: 2013/10/03 21:20:40 by lverniss          #+#    #+#             */
+/*   Updated: 2013/10/03 21:30:35 by lverniss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
-
-void	ft_putstr(char *str)
+int		ft_atoi(char *str)
 {
+	int		neg;
+	int		returned_value;
 	int		cn;
 
 	cn = -1;
-	while(str[++cn] != '\0')
+	neg = 1;
+	returned_value = 0;
+	while (str[++cn] == '-')
 	{
-		putchar(str[cn]);
+		neg = -1;
 	}
+	cn--;
+	while (str[++cn] <= '9' && str[cn] >= '0')
+	{
+		returned_value *= 10;
+		returned_value += (str[cn] - '0');
+	}
+	return (returned_value * neg);
 }
